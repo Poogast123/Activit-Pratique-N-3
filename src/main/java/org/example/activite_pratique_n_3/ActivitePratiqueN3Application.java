107e10760e720513659be6c62dd748ba93d8ff81
kharifi oussama <oussamakharifi2003@gmail.com>
2025-03-17 10:07:38 +0000

    first commit

---
+++ src/main/java/org/example/activite_pratique_n_3/ActivitePratiqueN3Application.java
@@ -0,0 +1,13 @@
+package org.example.activite_pratique_n_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitePratiqueN3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitePratiqueN3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/activite_pratique_n_3/ActivitePratiqueN3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.activite_pratique_n_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitePratiqueN3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
