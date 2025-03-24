a5ed4f55adaa1aebe9ca291e8552b2e7bac7a37a
kharifi oussama <oussamakharifi2003@gmail.com>
2025-03-24 10:12:41 +0000

    4th commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Activite_Pratique_N_3
+
+1. Initialiser le projet Spring Boot
+
+![img.png](img.png)
+
+
+2. Configurer la base de données
+
--- src/main/java/org/example/activite_pratique_n_3/PatientRepository.java
+++ src/main/java/org/example/activite_pratique_n_3/PatientRepository.java
@@ -1,10 +1,10 @@
 package org.example.activite_pratique_n_3;
 
-import org.example.activite_pratique_n_3.Patient;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    List<Patient> findByNameContaining(String keyword);
+    List<Patient> findByNameContaining(String keyword, PageRequest pageRequest);
 }
